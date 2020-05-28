1af9c43670b8d91df5d9b3c6856c38bf42c0f14e
ono shinji <yjsnpi555@gmail.com>
2020-05-28 18:11:37 +0900

    users,posts,sessions,favoritesコントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_074045) do
+ActiveRecord::Schema.define(version: 2020_05_28_090332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_074045) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
   end
 
   create_table "users", force: :cascade do |t|

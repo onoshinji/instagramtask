769f95497d4d5b27d3c43b417848a2dfe6d28712
ono shinji <yjsnpi555@gmail.com>
2020-05-29 10:06:25 +0900

    ログイン、ログアウト機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,3 @@
 class PostsController < ApplicationController
-  def new
-  end
-end
+
+ end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 10 }
-  validates :email, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 30 },
+  # メールアドレスが正しい形式で入力されているかのバリデーション
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
-    # メールアドレスが正しい形式で入力されているかのバリデーション
-  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
     # 大文字小文字を区別するためにすべて小文字として判定するための破壊的メソッドdowncase!
   before_validation { email.downcase! }
   has_secure_password
   has_many :favorites, dependent: :destroy
   # ユーザーはポストに対しても　１対多の関係により、hasmanyが必要という解釈。
-  has_many :posts, deoendent: :destroy 
+  #関連するものすべてを消去するためにdependent:destroys
+  has_many :posts, dependent: :destroy
 end

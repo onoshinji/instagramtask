f5a93633ac8f7235aacb3ec4fd17b7ddf4167d99
ono shinji <yjsnpi555@gmail.com>
2020-05-29 20:05:30 +0900

    404,500ページの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    # ここでユーザーの認証を行う。ユーザーが存在し、かつパラムス
     if user && user.authenticate(params[:session][:password])
+      # ブラウザのcookieにハッシュ化したユーザーidを保存
       session[:user_id] = user.id
+      # 個人ユーザーページにリダイレクトする
+      # user_pathで引数としてユーザのIDを持たせる
       redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
@@ -15,6 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    # ここはセッションに対してdelete
     session.delete(:user_id)
     flash[:notice] = 'ログアウトしました'
     redirect_to new_session_path

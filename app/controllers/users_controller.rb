88728673c4ba123a456abbe3fe16d070b9bf0dd0
ono shinji <yjsnpi555@gmail.com>
2020-06-03 16:47:38 +0900

    ユーザー編集機能を追加しました。,リサイズ機能はまだつけていません

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice: "ユーザー情報を登録しました"
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "ユーザー情報を更新したやで"
+    else
+      render :edit
     end
   end
   def show
@@ -16,6 +28,6 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                               :password_confirmation,:image)
+                               :password_confirmation,:image,:image_cache)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy,:show]
-  resources :users, only: [:new, :create, :destroy,:show]
+  resources :users
   resources :posts do
     collection do
       post :confirm

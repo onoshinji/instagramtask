3744879da9db9de6673e794679c047f92f164096
ono shinji <yjsnpi555@gmail.com>
2020-06-05 15:04:35 +0900

    お気に入り一覧ページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy,:show]
   resources :users
   # お気に入りの保存と削除するためのルーティング
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:new, :show, :index, :create, :destroy ]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :posts do
     collection do

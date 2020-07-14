aebc04b9cc25f152222dfef9f09c396c7c4ceb6a
ono shinji <yjsnpi555@gmail.com>
2020-07-14 20:09:15 +0900

    S３のバケット名を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
   case Rails.env
     when 'production'
-      config.fog_directory = 'ono-instagram-production'
+      config.fog_directory = 'ono-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ono-instagram_production'
     when 'development'
       config.fog_directory = 'ono-instagram-development'
